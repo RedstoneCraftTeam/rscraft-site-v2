8ee37f75cd39067e811ac91c1f8b364949b9ecd3
gene-2012 <gene201248@126.com>
2024-07-18 10:51:02 +0800

    完成download:releases页面

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 app_name = 'main'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('contract/', views.contract, name='contract'),
 ]
