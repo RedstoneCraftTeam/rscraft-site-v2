1527694304568bc830100c452681808cf03d8d4f
gene-2012 <gene201248@126.com>
2024-07-19 08:53:40 +0800

    重写Markdown渲染方式

--- MdPages/views.py
+++ MdPages/views.py
@@ -4,5 +4,5 @@ from .models import MarkdownFile
 def page(request, page_name):
     selected_page = MarkdownFile.objects.get(page_name=page_name)
     file_content = selected_page.file_path.read().decode('utf-8')
-    file_content = file_content.replace('\r\n', '\\n')
+    #file_content = file_content.replace('\r\n', '<p></p>')
     return render(request, 'pages/page.html', {'page_name': page_name, 'file_content': file_content})
